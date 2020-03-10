78fa90c096443cd2cc383e4d23c2515bcf9d5b56
Regina Compton <reginafcompton@gmail.com>
2020-03-10 11:30:40 -0400

    Testing ky (#17)
    * chore: Add pdb
    
    * More logging
    
    * chore: Precise logging for timestamps

--- etl/helpers/salesforce.py
+++ etl/helpers/salesforce.py
@@ -35,6 +35,7 @@ def parse_sf_record(nested_dict: Dict) -> Dict:
 def _get_sf_records(sf_hook: SalesforceHook, query: str) -> pd.DataFrame:
     query_result = sf_hook.make_query(query)
     records = [parse_sf_record(record) for record in query_result["records"]]
+
     df = pd.DataFrame.from_records(records)
     return df
 
@@ -50,9 +51,8 @@ def airflow_extract_data(
     """Extracts data from Salesforce that was modified between the start date
     and the execution date.
     """
+
     member_id = kwargs["task_instance"].xcom_pull(**get_member_xcom_args)
-    logging.info("Pulled a member id from `get_member` task.")
-    logging.info(member_id)
 
     CONN_ID = cms_info["connection_id"]
     QUERIES = cms_info["queries"]
@@ -60,22 +60,35 @@ def airflow_extract_data(
     sf_hook: SalesforceHook = SalesforceHook(conn_id=CONN_ID)
     docs_service = drive.get_google_docs_service(drive_credentials)
 
+    logging.info("execution_date")
+    logging.info(execution_date)
+
     start_datetime, end_datetime = dates.airflow_get_date_range(
         member_id, start_date, execution_date
     )
+
+    logging.info("Calculated start_datetime")
+    logging.info(start_datetime)
+    logging.info("Calculated end_datetime")
+    logging.info(end_datetime)
 
     filenames: List[str] = []
 
     for document_id in QUERIES:
         # Load the query and replace the dates
         query: str = drive.load_doc_as_query(docs_service, document_id)
+
         template = jinja2.Template(query)
         query_with_dates: str = template.render(
             start_datetime=f"{start_datetime}Z", end_datetime=f"{end_datetime}Z"
         )
 
+        logging.info("Query with populated WHERE clause:")
+        logging.info(query_with_dates)
+
         # Get the data and write it to a .csv
         records_dataframe: pd.DataFrame = _get_sf_records(sf_hook, query_with_dates)
+
         if not records_dataframe.empty:
             tf = tempfile.NamedTemporaryFile(delete=False)
             records_dataframe.to_csv(tf.name, index=False)
